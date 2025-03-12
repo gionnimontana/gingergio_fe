5e185fff4064d40a3c2cd57f8169b3e1ee0317bf
gionnimontana <gionnimontana@gmail.com>
2025-03-12 17:27:04 +0100

    feat: update email notification logic to include basket details in the message

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,16 +1,13 @@
-onRecordAfterCreateSuccess((e) => {
-    console.log("New record created", JSON.stringify(e))
-    // const isAnonymous = e.record.anonymous
-    // const sender = isAnonymous ? "Anonymous" : e.record.email()
-    // const message = new MailerMessage({
-    //     from: {
-    //         address: e.app.settings().meta.senderAddress,
-    //         name:    e.app.settings().meta.senderName,
-    //     },
-    //     to:      ['gionnimontana@gmail.com', 'gingergiobeverage@gmail.com'],
-    //     subject: "Nuovo Ordine da " + sender,
-    //     html:    JSON.stringify(e.record),
-    // })
+// e example payload :New record created {"event":{},"app":{},"record":{"address":"","basket":{"Zen Cola-Vetro 200ml":2},"collectionId":"pbc_3527180448","collectionName":"orders","confirmed":false,"created":"2025-03-12 16:23:34.287Z","delivered":false,"desired_delivery":"2025-03-16 16:23:00.000Z","id":"5x7vmx961p6wmv1","name":"sdaf","notes":"","surname":"","type":"onsite","updated":"2025-03-12 16:23:34.287Z","updates":"","user":"ugaj154q2oerw91"},"context":{},"type":"create"}
 
-    // e.app.newMailClient().send(message)
+onRecordAfterCreateSuccess((e) => {
+    const sender = e.record.name + " " + e.record.surname
+    const basket = JSON.stringify(e.record.basket)
+    const message = new MailerMessage({
+        to:      ['gionnimontana@gmail.com', 'gingergiobeverage@gmail.com'],
+        subject: "Nuovo Ordine da " + sender,
+        html: basket,
+    })
+
+    e.app.newMailClient().send(message)
 }, "orders")
