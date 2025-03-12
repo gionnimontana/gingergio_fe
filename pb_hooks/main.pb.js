a0e98a27367d5bed0e91c9243d7b3ffdefdff9b0
gionnimontana <gionnimontana@gmail.com>
2025-03-12 17:13:49 +0100

    feat: implement email notification for new order creation

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,4 +1,15 @@
 onRecordAfterCreateSuccess((e) => {
-    console.log("funzionaaaaa...")
-    e.next()
+    const isAnonymous = e.record.anonymous
+    const sender = isAnonymous ? "Anonymous" : e.record.email()
+    const message = new MailerMessage({
+        from: {
+            address: e.app.settings().meta.senderAddress,
+            name:    e.app.settings().meta.senderName,
+        },
+        to:      ['gionnimontana@gmail.com', 'gingergiobeverage@gmail.com'],
+        subject: "Nuovo Ordine da " + sender,
+        html:    JSON.stringify(e.record),
+    })
+
+    e.app.newMailClient().send(message)
 }, "orders")
