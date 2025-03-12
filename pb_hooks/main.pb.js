e1b1d5e1bb4565257b5287a3ea50a7a36d1dd423
gionnimontana <gionnimontana@gmail.com>
2025-03-12 17:21:52 +0100

    refactor: comment out email notification logic and add logging for new record creation

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,15 +1,16 @@
 onRecordAfterCreateSuccess((e) => {
-    const isAnonymous = e.record.anonymous
-    const sender = isAnonymous ? "Anonymous" : e.record.email()
-    const message = new MailerMessage({
-        from: {
-            address: e.app.settings().meta.senderAddress,
-            name:    e.app.settings().meta.senderName,
-        },
-        to:      ['gionnimontana@gmail.com', 'gingergiobeverage@gmail.com'],
-        subject: "Nuovo Ordine da " + sender,
-        html:    JSON.stringify(e.record),
-    })
+    console.log("New record created", e)
+    // const isAnonymous = e.record.anonymous
+    // const sender = isAnonymous ? "Anonymous" : e.record.email()
+    // const message = new MailerMessage({
+    //     from: {
+    //         address: e.app.settings().meta.senderAddress,
+    //         name:    e.app.settings().meta.senderName,
+    //     },
+    //     to:      ['gionnimontana@gmail.com', 'gingergiobeverage@gmail.com'],
+    //     subject: "Nuovo Ordine da " + sender,
+    //     html:    JSON.stringify(e.record),
+    // })
 
-    e.app.newMailClient().send(message)
+    // e.app.newMailClient().send(message)
 }, "orders")
